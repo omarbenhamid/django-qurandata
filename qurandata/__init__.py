fa31ff6d39637310976e79494656aeb5e7a4926b
Omar BENHAMID <contact@obenhamid.me>
2018-05-11 07:01:00 +0100

    Adding decoding of names of ayarefs

--- qurandata/__init__.py
+++ qurandata/__init__.py
@@ -2,16 +2,33 @@ import re
 
 class AyaRef:
     def __init__(self, reftxt, contextsura=None):
-        m = re.match(r'(?:S(?P<sura>[0-9]+))?A(?P<startaya>[0-9]+)(?::A?(?P<endaya>[0-9]+))?',reftxt)
+        m = re.match(r'(?:S(?P<singlesura>[0-9]+)|(?:S(?P<sura>[0-9]+))?A(?P<startaya>[0-9]+)(?::A?(?P<endaya>[0-9]+))?)',reftxt)
         self.expr = reftxt
-        self.startaya=int(m.group('startaya'))
+        ssura = m.group('singlesura')
+        if ssura:
+            self.fullsura = True
+            self.suraidx = int(ssura)
+            self.startaya = 1
+            self._endaya = None
+            return
+        self.fullsura = False
         sura = m.group('sura')
-        endaya = m.group('endaya')
-        self.endaya = int(endaya) if endaya else self.startaya
         self.suraidx = int(sura) if sura else contextsura
-    
+        self.startaya=int(m.group('startaya'))
+        endaya = m.group('endaya')
+        self._endaya = int(endaya) if endaya else self.startaya
+        
     def __repr__(self):
-        return "<AyaRef %s (sura %r, ayas %r-%r)>" % (self.expr, self.suraidx, self.startaya, self.endaya)
+        if self.fullsura:
+            return '<AyaRef %s (sura %r)>' % (self.expr, self.suraidx)
+        else:
+            return "<AyaRef %s (sura %r, ayas %r-%r)>" % (self.expr, self.suraidx, self.startaya, self._endaya)
+    
+    @property
+    def endaya(self):
+        if self._endaya == None and self.suraidx:
+            self._endaya = models.Sura.objects.get(index=self.suraidx).ayas
+        return self._endaya
     
     def isabsolute(self):
         return self.suraidx != None
@@ -23,13 +40,23 @@ class AyaRef:
         if self.suraidx == None: raise Exception("Cannot get ayas of relative reference")
         return models.Aya.objects.filter(sura__index=self.suraidx, index__range=(self.startaya, self.endaya)).all()
     
+    def ashtml(self):
+        if self.fullsura:
+            return '<span dir="rtl" title="%s">%s</span>' % (self.expr,
+                models.Sura.objects.get(index=self.suraidx).name)
+        else:
+            return '<span dir="rtl" title="%s">%s من <span class="citation">%d</span> إلى <span class="citation">%d</span></span>' % (self.expr,
+                models.Sura.objects.get(index=self.suraidx).name if self.suraidx else '',
+                self.startaya, self.endaya)
+    
 def _parserefs(text, suraidx=None):
     ret = []
-    for ref in re.findall(r'\b((?:S[0-9]+)?A[0-9]+(?::A?[0-9]+)?)\b',text):
+    for ref in re.findall(r'\b(S[0-9]+|(?:S[0-9]+)?A[0-9]+(?::A?[0-9]+)?)\b',text):
         r = AyaRef(ref, contextsura=suraidx)
         if not r.isabsolute(): continue
         ret.append(r)
         suraidx = r.suraidx
+    
     return ret
 
 def refs_from_text(text, context=""):
@@ -41,6 +68,7 @@ def refs_from_text(text, context=""):
             - A3 is aya 3 of last surah identified in text (or in contexttext)
             - S1A2:5 or S1A2:A5 is ayat form aya 2 to aya 5 of surah 1
     """
+    if context==None: context=""
     ayarefs = _parserefs(context)
     suraidx = ayarefs.pop() if len(ayarefs) > 0 else None
     return _parserefs(text, suraidx)
