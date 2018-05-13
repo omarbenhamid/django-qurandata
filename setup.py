96b0afd009e04f15e1a34cd839b4c45113445f11
Omar BENHAMID <contact@obenhamid.me>
2018-05-13 15:26:06 +0000

    Avoiding main readme in setup.py to avoid encoding issues on some plateforms

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+README = "Quran data for django"
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__),
